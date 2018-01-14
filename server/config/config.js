11c627ab753ea5797354de5d8d755b7a5f8790e2
Wojtas <wojtas@wojtas.net.pl>
2018-01-14 15:11:48 +0100

    update config

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,12 @@
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/Sinara';
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/SinaraTest';
+if (env === 'test' || env === 'development') {
+  var config = require('./config.json');
+  var envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
 }
 
 module.exports = {env};
